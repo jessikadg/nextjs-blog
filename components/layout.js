2f5a1e71ab05db31b928c85fd3b522657c34c3c9
Jessika <jessikadg@gmail.com>
2023-05-01 12:41:22 +0200

    Fixed Title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Hello World! Hello";
+const name = "Hello World!";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
